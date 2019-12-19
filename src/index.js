4a43339cbd8a64098dad914034507c68c0a2db1b
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2019-12-19 10:15:22 -0600

    navbar added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './index.css';
+import 'gestalt/dist/gestalt.css';
 
 //Components
 import App from './components/App';
 import Signin from './components/signin';
 import Signup from './components/signup';
 import Checkout from './components/checkout';
+import Navbar from './components/navbar';
 //End Components
 
 import * as serviceWorker from './serviceWorker';
 
 const Root = () => (
     <Router>
+        <Navbar />
         <Switch>
             <Route component={App} exact path="/" />
             <Route component={Signin} path="/signin" />
