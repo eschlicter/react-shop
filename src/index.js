e936a7adf2867334f03d72c7bf4a6d7b5ed7cdbd
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2019-12-19 09:58:00 -0600

    added checkout, signin, signup components and their routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
+
+//Components
 import App from './components/App';
+import Signin from './components/signin';
+import Signup from './components/signup';
+import Checkout from './components/checkout';
+//End Components
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route component={App} exact path="/" />
+            <Route component={Signin} path="/signin" />
+            <Route component={Signup} path="/signup" />
+            <Route component={Checkout} path="/checkout" />
+        </Switch>
+    </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
